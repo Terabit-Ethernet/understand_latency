8d4d5c0595a0a1936414bde64d9f610e81821377
root <Jaehyun Hwang>
2023-01-11 06:50:46 +0000

    add change

--- pingpong_server.cc
+++ pingpong_server.cc
@@ -166,7 +166,7 @@ void nd_pingpong()
 	// setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
 	if (getsockname(fd, (struct sockaddr *)&sin, &len) == -1)
 	    perror("getsockname");
-	printf("pid:%d port number %d\n", pid, ntohs(sin.sin_port));
+	printf("pid:%d port number %d\n", pid, ntohs(source.sin_port));
 	// start_cycle = rdtsc();
 	setsockopt(fd, SOL_SOCKET, SO_PRIORITY, &optval, unsigned(sizeof(optval)));   
 	getsockopt(fd, SOL_SOCKET, SO_PRIORITY, &optval, &optlen);
