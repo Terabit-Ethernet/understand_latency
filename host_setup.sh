a5e1aca007e53c93efe4ab11e6217c1cb60067cd
Qizhe <qizhe.cai@gmail.com>
2023-09-19 02:02:59 +0000

    add host setup

--- host_setup.sh
+++ host_setup.sh
@@ -24,10 +24,10 @@ sudo hwstamp_ctl -i $INTF -r 1
 #echo HRTICK | sudo tee /sys/kernel/debug/sched_features
 #echo 100000 | sudo tee /proc/sys/kernel/sched_latency_ns
 #echo 100000 | sudo tee /proc/sys/kernel/sched_min_granularity_ns
-#sudo phc2sys -s CLOCK_REALTIME -c $INTF -O 0 &
+sudo phc2sys -s CLOCK_REALTIME -c $INTF -O 0 &
 
 # synchoronize the hardware timer
-#phc2sys -a -r
+# phc2sys -a -r
 # comppile compute app
 #g++ -pthread compute_md.cpp -o compute
 
