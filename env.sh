e15170795267be01a28fb096bcdef24d384ac53e
amefumi <zty326@hotmail.com>
2026-09-05 22:59:32 -0400

    sigcomm-ae: basement

--- env.sh
+++ env.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-HOST=192.168.11.124
-TARGET=192.168.11.125
-INTF=ens2f1
-USER=jaehyun
-TARGETDIR=/home/qizhe/
-TARGETC=128.84.155.146
-NVME_TCP_DEV=/dev/nvme1n1
-I10_DEV=/dev/nvme2n1
-NUM_CPUS=32
+
+# Set to client-side IP address
+HOST=192.168.1.101
+
+# Set to server-side IP address
+TARGET=192.168.1.102
+
+# Set to the network interface name (eg. ens1np0).
+# We assume both side use the same interface name. 
+# Please change other scripts accordingly if they are different.
+INTF=enp202s0f0np0
+
+# Set to the hostname of the remote server (e.g., syslab.cs.virginia.edu)
+TARGETC=clnode264.clemson.cloudlab.us
+
+# Username of the remote server (e.g., netian)
+USER=netian
+
+# Set to the directory of the project on the remote server (e.g., /home/ame)
+TARGETDIR=/users/netian
