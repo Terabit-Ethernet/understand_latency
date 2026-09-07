6a8bdf80b5a3e961d15364449a20a8fce1b249b8
amefumi <zty326@hotmail.com>
2026-09-07 13:02:48 -0400

    sigcomm-ae: isolated case heatmap

--- parse/parse_single_core_latency_throughput.py
+++ parse/parse_single_core_latency_throughput.py
@@ -23,7 +23,7 @@ import numpy as np
 # Configuration:
 result_dir = "/data/projects/latency"
 # Experiments to parse. Leave empty to parse every experiment found in result_dir.
-experiments = ["isolated_thread_default"]
+experiments = ["single_core_macro_default"]
 
 total_bin = 100000
 
